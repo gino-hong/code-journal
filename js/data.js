c723cfe49171e32aa65ecdbbabb87165bd22e067
Gino Hong <ginohong7@gmail.com>
2022-03-29 20:56:16 -0700

    Added event listeners. #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,19 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var $form = document.querySelector('form');
+var $image = document.querySelector('img');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var object = {};
+  object.title = $form.elements.title.value;
+  object.photoUrl = $form.elements.photoUrl.value;
+  object.notes = $form.elements.notes.value;
+  object.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(object);
+  $form.reset();
+  $image.src = 'images/placeholder-image-square.jpg';
+});
--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#photoUrl');
+var $image = document.querySelector('img');
+
+$photoUrl.addEventListener('input', function (e) {
+  $image.src = e.target.value;
+});
