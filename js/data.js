c58a492fcd5c07690d902958378bb3fc5fd9c20b
Gino Hong <ginohong7@gmail.com>
2022-03-31 19:06:09 -0700

    Moved code from data.js to main.js so that data.js only contains local storage functionality and main data object.

--- js/data.js
+++ js/data.js
@@ -12,22 +12,6 @@ if (previousDataJSON !== null) {
   data = JSON.parse(previousDataJSON);
 }
 
-var $form = document.querySelector('form');
-var $image = document.querySelector('img');
-
-$form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  var object = {};
-  object.title = $form.elements.title.value;
-  object.photoUrl = $form.elements.photoUrl.value;
-  object.notes = $form.elements.notes.value;
-  object.entryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.push(object);
-  $form.reset();
-  $image.src = 'images/placeholder-image-square.jpg';
-});
-
 window.addEventListener('beforeunload', function (e) {
   var dataJSON = JSON.stringify(data);
   this.localStorage.setItem('data-local-storage', dataJSON);
--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ $photoUrl.addEventListener('input', function (e) {
 function isImage(url) {
   return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
 }
+
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var object = {};
+  object.title = $form.elements.title.value;
+  object.photoUrl = $form.elements.photoUrl.value;
+  object.notes = $form.elements.notes.value;
+  object.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(object);
+  $form.reset();
+  $image.src = 'images/placeholder-image-square.jpg';
+});
