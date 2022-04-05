daa895907fad95f5d71e4c21f501a719bac16935
Gino Hong <ginohong7@gmail.com>
2022-04-04 21:51:00 -0700

    refreshing pages shows same view as before refreshing. #2

--- js/data.js
+++ js/data.js
@@ -12,7 +12,21 @@ if (previousDataJSON !== null) {
   data = JSON.parse(previousDataJSON);
 }
 
+var $entryForm = document.querySelector('#entry-form');
+var $entries = document.querySelector('#entries');
+
+var previousEntryFormClass = localStorage.getItem('entry-form-class');
+if (previousEntryFormClass !== null) {
+  $entryForm.className = previousEntryFormClass;
+}
+var previousEntriesClass = localStorage.getItem('entries-class');
+if (previousEntriesClass !== null) {
+  $entries.className = previousEntriesClass;
+}
+
 window.addEventListener('beforeunload', function (e) {
   var dataJSON = JSON.stringify(data);
   this.localStorage.setItem('data-local-storage', dataJSON);
+  this.localStorage.setItem('entry-form-class', $entryForm.className);
+  this.localStorage.setItem('entries-class', $entries.className);
 });
