e3ee472e2844610ce2a3d02e8329d53b825e1d78
Gino Hong <ginohong7@gmail.com>
2022-04-05 21:01:42 -0700

    Ensure that each rendered entry is given a data-entry-id attribute indicating which entry it is. #3

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@ $form.addEventListener('submit', function (event) {
 
 function renderEntry(entry) {
   var listItem = document.createElement('li');
+  listItem.setAttribute('data-entry-id', entry.entryId);
   listItem.setAttribute('class', 'container mb20');
 
   var rowDiv = document.createElement('div');
